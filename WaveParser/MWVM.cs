76974a5065c973493f91dcac9063d94d51b5fb7b
Mentat <indraua@gmail.com>
2014-10-15 23:05:05 +0300

    Graphic drawing

--- WaveParser/MWVM.cs
+++ WaveParser/MWVM.cs
@@ -1,44 +1,38 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace WaveParser
 {
     public class MWVM
     {
-        public ICommand LoadFile;
-
-        private MainWindow _window;
-        private string _path = "";
+        private readonly MainWindow _window;
 
         public MWVM(MainWindow window)
         {
             _window = window;
-
-            LoadFile = new DelegateCommand(o =>
-                {
-                    using (var sr = new StreamReader(_path))
-                    {
-                        
-                    }
-                });
-            DrawLine(0, 0, 100, 100, 1);
         }
 
-        public void DrawLine(int x1, int y1, int x2, int y2, int thickness)
+        public PointCollection GraphicPoints
         {
-            Line line = new Line();
-            line.X1 = x1;
-            line.Y1 = y1;
-            line.X2 = x2;
-            line.Y2 = y2;
-            line.Stroke = Brushes.Black;
-            line.StrokeThickness = thickness;
-            line.Tag = string.Format("Line {0} {1} {2} {3}", x1, y1, x2, y2);
-            _window.Canvas.Children.Add(line);
+            get
+            {
+                return
+                    new PointCollection(new[]
+                    {
+                        new Point(0, 0),            
+                        new Point(10, 30), 
+                        new Point(20, 20), 
+                        new Point(30, 40), 
+                        new Point(40, 90),
+                        new Point(50, 180),
+                        new Point(60, 2),
+                        new Point(70, 31),
+                        new Point(80, 90),
+                        new Point(90, 432),
+                        new Point(100, 13),
+                        new Point(110, 2),
+                    });
+            }
         }
     }
 }
