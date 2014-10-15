dc7447535b817fa407c7e1fcc893df1c7c8dd5f3
darkkat <m.steetch@gmail.com>
2014-10-15 23:21:40 +0300

    fckn fix

--- WaveParser/MWVM.cs
+++ WaveParser/MWVM.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace WaveParser
 {
@@ -32,28 +34,29 @@ namespace WaveParser
                 });
             DrawLine(0, 0, 100, 100, 1);
         }
-        
-    public PointCollection GraphicPoints
+
+        public PointCollection GraphicPoints
         {
             get
             {
                 return
                     new PointCollection(new[]
-                    {
-                        new Point(0, 0),            
-                        new Point(10, 30), 
-                        new Point(20, 20), 
-                        new Point(30, 40), 
-                        new Point(40, 90),
-                        new Point(50, 180),
-                        new Point(60, 2),
-                        new Point(70, 31),
-                        new Point(80, 90),
-                        new Point(90, 432),
-                        new Point(100, 13),
-                        new Point(110, 2),
-                    });
+                        {
+                            new Point(0, 0),
+                            new Point(10, 30),
+                            new Point(20, 20),
+                            new Point(30, 40),
+                            new Point(40, 90),
+                            new Point(50, 180),
+                            new Point(60, 2),
+                            new Point(70, 31),
+                            new Point(80, 90),
+                            new Point(90, 432),
+                            new Point(100, 13),
+                            new Point(110, 2),
+                        });
             }
+        }
 
         public void DrawLine(int x1, int y1, int x2, int y2, int thickness)
         {
